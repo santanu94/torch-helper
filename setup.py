8eea6b1760c24409768818569c7b552c8e4987c8
Santanu Bhattacharjee <mail.santanu94@gmail.com>
2020-07-24 04:28:37 +0530

    setup.py url update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A pytorch utility to provide customizable quick use functions.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/santanu94/torch-helper",
+    url="https://github.com/santanu94/torch-helper.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
