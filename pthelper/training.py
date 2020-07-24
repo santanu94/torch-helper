316e1a5611e93e9d4223f135de7f75f31dc4cf43
Santanu Bhattacharjee <mail.santanu94@gmail.com>
2020-07-24 06:06:05 +0530

    Added getter functions for opt and criterion. Added print model summary.

--- pthelper/training.py
+++ pthelper/training.py
@@ -9,16 +9,26 @@ class ModelWrapper():
         self.__state_data['opt'] = opt
         self.__state_data['criterion'] = criterion
         self.__state_data['total_trained_epochs'] = 0
-        self.__state_data['best_val_loss'] = null
+        self.__state_data['best_val_loss'] = None
         self.__state_data['history'] = { 'train_loss': [], 'val_loss': [], 'val_acc': [] }
         self.__state_data['model'] = to_device(model, device if device else get_default_device())
         self.watch = watch
 
+    # Setter methods
     def set_optimizer(self, opt):
         self.__state_data['opt'] = opt
 
     def set_criterion(self, criterion):
         self.__state_data['criterion'] = criterion
+
+    # Getter methods
+    def optimizer(self):
+        """Getter function for optimizer"""
+        print(self.__state_data['opt'])
+
+    def criterion(self):
+        """Getter function for criterion"""
+        print(self.__state_data['criterion'])
 
     def fit(self, epoch, train_dl, val_dl, test_dl=None, save_best_model_policy='val_loss'):
         """
@@ -122,3 +132,6 @@ class ModelWrapper():
     def untrained_model_stats(self, val_dl):
         mean_epoch_val_loss, mean_epoch_val_acc = self.__validation_step(val_dl)
         print('initial val loss ->', mean_epoch_val_loss, '  initial val acc ->', mean_epoch_val_acc)
+
+    def model_summary(self):
+        print(self.__state_data['model'])
