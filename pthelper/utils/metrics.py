bcff7c2979ec906f2afcfea046a5e765406809e1
Santanu Bhattacharjee <mail.santanu94@gmail.com>
2020-08-12 10:42:14 +0530

    Bug fix

--- pthelper/utils/metrics.py
+++ pthelper/utils/metrics.py
@@ -8,9 +8,9 @@ def get_accuracy(y_true, y_pred):
 def get_f1_score(y_true, y_pred, average):
     print(y_pred.device)
     if torch.is_tensor(y_true):
-        y_true = y_true.cpu()
+        y_true = y_true.cpu().detach()
     if torch.is_tensor(y_pred):
-        y_pred = y_pred.cpu()
+        y_pred = y_pred.cpu().detach()
     if average in ['binary', 'micro', 'macro', 'weighted', 'samples']:
         return f1_score(y_true, y_pred, average=average)
     else:
